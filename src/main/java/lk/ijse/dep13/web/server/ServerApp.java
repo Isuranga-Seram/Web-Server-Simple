abb501e2d089273c49bdcfa8867711dc2405c0a3
Isuranga Seram <isurangaseram@gmail.com>
2025-01-27 11:15:19 +0530

    Initial-Commit-Add-README

---
+++ src/main/java/lk/ijse/dep13/web/server/ServerApp.java
@@ -0,0 +1,8 @@
+package lk.ijse.dep13.web.server;
+
+public class ServerApp {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+open module lk.ijse.dep13.web.server {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+}
