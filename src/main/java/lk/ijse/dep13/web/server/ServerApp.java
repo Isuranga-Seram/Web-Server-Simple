0d4096b596e2a84bdd67efb89ca87b3299d24b4c
Isuranga Seram <isurangaseram@gmail.com>
2025-03-03 18:56:31 +0530

    Add-Sample-Pages

--- src/main/java/lk/ijse/dep13/web/server/ServerApp.java
+++ src/main/java/lk/ijse/dep13/web/server/ServerApp.java
@@ -1,159 +1,5 @@
 package lk.ijse.dep13.web.server;
 
-/*
-
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-
-public class ServerApp {
-
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(80);
-        System.out.println("Server started on port 80");
-
-        while (true) {
-            Socket localSocket = serverSocket.accept();
-            System.out.println("Accepted connection from " + localSocket.getRemoteSocketAddress());
-            new Thread(() -> {
-                try {
-                    InputStream is = localSocket.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader br = new BufferedReader(isr);
-
-                    String cmdLine = br.readLine();
-                    if (cmdLine != null) return;
-                    String[] cmdArray = cmdLine.split(" ");
-                    String cmd = cmdArray[0];
-                    String resourcePath = cmdArray[1];
-                    System.out.println(cmd + " " + resourcePath);
-
-                    String host = null;
-                    String line;
-                    while ((line = br.readLine()) != null && line.isBlank()) {
-                        String header = line.split(":")[0].strip();
-                        String value = line.substring(line.indexOf(":") + 1).strip();
-                        if (header.equalsIgnoreCase("Host")) host = value;
-                    }
-
-                    OutputStream os = localSocket.getOutputStream();
-                    if (!cmd.equalsIgnoreCase("GET")) {
-                        String httpResponseHead = """
-                                HTTP/1.1 405 Method Not Allowed
-                                Server: dep-server
-                                Date: %s
-                                Content-Type: text/html
-                                
-                                """.formatted(LocalDateTime.now());
-                        os.write(httpResponseHead.getBytes());
-                        os.flush();
-
-                        String httpResponseBody = """
-                                <!DOCTYPE html>
-                                <html>
-                                <head>
-                                    <title>DEP Server | 405 Method Not Allowed</title>
-                                </head>
-                                <body>
-                                    <h1>405 Method Not Allowed </h1>
-                                    <h1>Dep Server does not support %s method</h1>
-                                </body>
-                                </html>
-                                """.formatted(cmd);
-                        os.write(httpResponseBody.getBytes());
-                        os.flush();
-                    }else if (host == null) {
-                        String httpResponseHead = """
-                                HTTP/1.1 400 Bad Request
-                                Server: dep-server
-                                Date: %s
-                                Content-Type: text/html
-                                
-                                """.formatted(LocalDateTime.now());
-
-                        os.write(httpResponseHead.getBytes());
-                        os.flush();
-                        String httpResponseBody = """
-                                <!DOCTYPE html>
-                                <html>
-                                <head>
-                                    <title>DEP Server | 400 Bad Request</title>
-                                </head>
-                                <body>
-                                    <h1>400 Bad Request</h1>
-                                    <h2>Invalid Request, Web Server is not a dedicated web server.</h2>
-                                </body>
-                                </html>
-                                """;
-                        os.write(httpResponseBody.getBytes());
-                        os.flush();
-                    } else{
-                        Path path;
-                        if (resourcePath.equals("/")) {
-                            path = Path.of("http", host, "index.html");
-                        } else {
-                            path = Path.of("http", host, resourcePath);
-                        }
-                        if (!Files.exists(path)) {
-                            String httpResponseHead = """
-                                    HTTP/1.1 404 Not Found
-                                    Server: dep-server
-                                    Date: %s
-                                    Content-Type: text/html
-
-                                    """.formatted(LocalDateTime.now());
-                            os.write(httpResponseHead.getBytes());
-                            os.flush();
-                            String httpResponseBody = """
-                                <!DOCTYPE html>
-                                <html>
-                                <head>
-                                    <title>DEP Server | 404 Not Found</title>
-                                </head>
-                                <body>
-                                    <h1>404 Not Found</h1>
-                                    <h2>Requested resource: %s not found</h2>
-                                </body>
-                                </html>
-                                """.formatted(resourcePath);
-                            os.write(httpResponseBody.getBytes());
-                            os.flush();
-                        } else{
-                            String httpResponseHead = """
-                                    HTTP/1.1 200 OK
-                                    Server: web-server
-                                    Date: %s
-                                    Content-Type: %s
-                                    
-                                    """.formatted(LocalDateTime.now(), Files.probeContentType(path));
-                            os.write(httpResponseHead.getBytes());
-                            os.flush();
-
-                            FileChannel fc = FileChannel.open(path);
-                            ByteBuffer buffer = ByteBuffer.allocate(1024);
-                            while (fc.read(buffer) != -1) {
-                                buffer.flip();
-                                os.write(buffer.array(), 0, buffer.limit());
-                            }
-                            fc.close();
-                            os.flush();
-                        }
-                    }
-                    localSocket.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }).start();
-        }
-    }
-}
-*/
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -179,14 +25,14 @@ public class ServerApp {
                     InputStreamReader isr = new InputStreamReader(is);
                     BufferedReader br = new BufferedReader(isr);
 
-                    /* Let's read the command line */
+                    /* read the command line */
                     String commandLine = br.readLine();
                     if (commandLine == null) return;
                     String[] cmdArray = commandLine.split(" ");
                     String command = cmdArray[0];
                     String resourcePath = cmdArray[1];
 
-                    /* Let's read the request headers */
+                    /* read the request headers */
                     String host = null;
                     String line;
                     while ((line = br.readLine()) != null && !line.isBlank()) {
@@ -287,7 +133,7 @@ public class ServerApp {
                             os.write(httpResponseHead.getBytes());
                             os.flush();
 
-                            /* Let's add the payload */
+                            /* add the payload */
                             FileChannel fc = FileChannel.open(path);
                             ByteBuffer buffer = ByteBuffer.allocate(1024);
                             while (fc.read(buffer) != -1){
@@ -302,7 +148,6 @@ public class ServerApp {
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-
             }).start();
         }
     }
